8e2a24b405c7b8e4d65b930567362faeaf05ab0c
deebuj <38571187+deebuj@users.noreply.github.com>
2018-05-30 10:25:35 -0500

    scss changes

---
+++ src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./_MainComponent.scss";
+
+class MainComponent extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Main Component</h2>
+      </div>
+    );
+  }
+}
+
+export default MainComponent;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import MainComponent from "./components/MainComponent";
 
 import "./styles.scss";
 
 const App = () => (
   <div>
     <h1>Hello</h1>
+    <h2>Some heading</h2>
+    <MainComponent />
   </div>
 );
 
